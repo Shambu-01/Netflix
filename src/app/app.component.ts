07398b7f49d241e076264b51de3a3654a82bea25
Shambu-01 <jakkula.shambhavi31@gmail.com>
2024-02-15 01:36:46 +0530

    update

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Netflix';
+  img1="https://occ-0-2042-3662.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABYIWwzftYFhohxbceaZGPYPr8na_RNwTBGu6pH6tCoTVg85wody4V4s1D3iP4UD1SiT7cN6PKrQ_reZ9pRA6YDOrKyIxAFgWR68.webp?r=01d";
+  img2="https://occ-0-2042-3662.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABSRyL0AUCvLcJLxYXFj7o0fO-vaTWuUnfgu3F8e97_AuaiPfZ5AlufK5N6o-bMcxJQf20JChatut5BDrDyObF-1VJKYI8WpWOR-GbMY2rjkIUIWLf6UOl08NAFmqskc8Dvg9.jpg?r=090";
+  img3="https://occ-0-2042-3662.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABSDKfGxNrJO5asgLZRFOozA42fOFihS5PX9BHiOA2oh80Z0Q6VVjszQZJQBjuBdf71wYplhGiDf1S5FuZBCboFRF_uVZX3R4NPWbbeXAh763LZ-PMX02v5QIBY6DiMS23Oy1.jpg?r=a62";
+  img4="https://occ-0-2042-3662.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABcZnxCyoyk_ko8XEqHiURGSB_c03bt2edP2vhRwLMhUGovLiMXRAso9j9ICSbWR8sKTBrxw6EmjGFlIfIqoXCON_850fJL5gt2Y.webp?r=4fc";
+  img5="https://occ-0-2042-3662.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABaYf4twF9Wk0VvJumzaIbpvSSMW0x0kjWrxRUJOatMxjKHS5U1dcrnMSkrfurKEPRirxTKaj0LgdLOXUmetmNnE7rGC_jo-rL44.webp?r=6e5";
+}
